0651fd3e9ad487beb6dd370ff1e7a37176a17982
Ashutosh bansal <bansalashutosh77@gmail.com>
2024-10-14 19:44:53 +0530

    backend

---
+++ server/npm/index.js
@@ -0,0 +1,13 @@
+// const giveMeAJoke = require('give-me-a-joke');
+
+// // To get a random dad joke
+// giveMeAJoke.getRandomDadJoke (function(joke) {
+//      console.log(joke);
+// });
+
+const cowsay = require('cowsay');
+console.log(cowsay.say({
+    text: "Hello, I'm Divyanshu!",
+    e: "oO",
+    T: "U "
+}));
